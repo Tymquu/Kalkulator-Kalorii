73dca7c51cd04ffbaee25367e3184145121bbf6b
Tymu <tymekkrz@interia.eu>
2023-05-29 18:40:18 +0200

    Baza Danych V.1.1

---
+++ Kalkulator Kalorii/MVVM/Model/DataBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Kalkulator_Kalorii.MVVM.Model
+{
+    public class DataBase
+    {
+        private SQLiteConnection conn;
+        private string sql;
+        private SQLiteCommand command;
+
+        public SQLiteConnection Connection(string loc)
+        {
+            return conn = new SQLiteConnection($"Data Source={loc};Version=3");
+        }
+
+        public void Create_DB(string loc)
+        {
+
+            //Tworzenie bazy
+            SQLiteConnection.CreateFile(loc);
+
+            conn = new SQLiteConnection($"Data Source={loc};Version=3");
+            conn.Open();
+
+            //Tabela Urzytkownik
+            sql = "CREATE TABLE User (UserID int primary key,NazwaUzytkownika varchar(50),Wzrost int, Plec varchar(20),ObecnaWaga decimal(4,2),DocelowaWaga decimal(4,2)) ";
+            command = new SQLiteCommand(sql, conn);
+            command.ExecuteNonQuery();
+
+            //Tabela Historia
+            sql = "CREATE TABLE Historia (HistoriaID int primary key, UserID int, PosilekTyp varchar(30), DanyPosilek varchar(30), WagaPosilku int, KalorycznoscPosilku int, Woda int, Aktywnosc varchar(30), CzasAktywnosci varchar(30), Data text, foreign key(UserID) references User(UserID)) ";
+            command = new SQLiteCommand(sql, conn);
+            command.ExecuteNonQuery();
+
+
+            //Pierwszy wpis testowy
+            sql = @"insert into User values(1,'Eustachy',178, 'h', 0.1, 0.2); ";
+            command = new SQLiteCommand(sql, conn);
+            command.ExecuteNonQuery();
+
+            sql = @"insert into Historia values(1,1,'Śniadanie', 's', 21, 37, 'g', '55', '77'); ";
+            command = new SQLiteCommand(sql, conn);
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+    }
+}
--- Kalkulator Kalorii/MVVM/View/Historia.xaml.cs
+++ Kalkulator Kalorii/MVVM/View/Historia.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Kalkulator_Kalorii.MVVM.Model;
 
 namespace Kalkulator_Kalorii.MVVM.View
 {
@@ -19,9 +23,51 @@ namespace Kalkulator_Kalorii.MVVM.View
     /// </summary>
     public partial class Historia : Window
     {
+        private SQLiteConnection conn;
+        private SQLiteCommand command;
+        private string sql;
+
         public Historia()
         {
             InitializeComponent();
+            Load();
+        }
+
+        public void Load()
+        {
+            string lokalizacja;
+            DataBase db = new DataBase();
+            lokalizacja = "C:\baza.sqlite";
+#if DEBUG
+            lokalizacja = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\baza.sqlite";
+#endif
+
+
+            if (!File.Exists(lokalizacja))
+            {
+                db.Create_DB(lokalizacja);
+            }
+
+            conn = db.Connection(lokalizacja);
+            conn.Open();
+
+            sql = "SELECT * FROM Historia";
+            command = new SQLiteCommand(sql, conn);
+
+            Fill(command);
+            conn.Close();
+        }
+
+        public void Fill(SQLiteCommand command)
+        {
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                Historia_kalorii.ItemsSource = ds.Tables[0].DefaultView;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
