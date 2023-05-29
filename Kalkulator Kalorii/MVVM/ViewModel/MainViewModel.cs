491d47d7e8d7c10558a573d4b91f45f01fe5d626
Tymu <tymekkrz@interia.eu>
2023-05-29 18:39:56 +0200

    MainViewModel + helpers

---
+++ Kalkulator Kalorii/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Kalkulator_Kalorii.Core;
+using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Kalkulator_Kalorii.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private string _user;
+        public string User { 
+            get { 
+                return _user; 
+            } 
+            set { 
+                _user = value; 
+                OnPropertyChanged();
+            } 
+        }
+
+        private ComboBoxItem _profile;
+        public ComboBoxItem Profile
+        {
+            get
+            {
+                return _profile;
+            }
+            set
+            {
+                _profile = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ICommand _setUsername;
+        public ICommand SetUsername => _setUsername ?? (_setUsername = new CommandHandler(() => SetUser(), () => CanExecute));
+
+        public bool CanExecute
+        {
+            get
+            {
+                // check if executing is allowed, i.e., validate, check if a process is running, etc. 
+                return true;
+            }
+        }
+
+        private void SetUser()
+        {
+            User = (string)Profile.Content;
+        }
+
+        public MainViewModel()
+        {
+ 
+        }
+    }
+}
