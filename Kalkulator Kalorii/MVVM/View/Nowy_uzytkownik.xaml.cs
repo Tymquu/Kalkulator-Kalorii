3b6c60b9b884917ff1f9d86e2541e6f76648f867
Tymu <tymekkrz@interia.eu>
2023-05-29 18:39:46 +0200

    GUI częściowo zrobione

---
+++ Kalkulator Kalorii/MVVM/View/Nowy_uzytkownik.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kalkulator_Kalorii.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for Wybor_uzytkownika.xaml
+    /// </summary>
+    public partial class Wybor_uzytkownika : Window
+    {
+        public Wybor_uzytkownika()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
